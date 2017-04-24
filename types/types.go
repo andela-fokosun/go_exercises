5599b1d63703155b93fd2eccd80c267b98a4cbbf
andela-fokosun <okosunuzflorence@gmail.com>
2017-04-24 16:51:45 +0100

    multi line declaration and initialisation

--- types/types.go
+++ types/types.go
@@ -9,9 +9,16 @@ func main() {
 	// string declaration
 	var greeting string
 
-	button = true
+	// multiple declarations on one line
+	var a, b, c int
+
+	// multiple declaration and initialisation
+	var d, e, f int = 1, 2, 3
+
 	greeting = "Hello World"
 
 	fmt.Println(button)
 	fmt.Println(greeting)
+	fmt.Println(a, b, c)
+	fmt.Println(d, e, f)
 }
