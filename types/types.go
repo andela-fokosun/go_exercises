7c4abb3978955134e9579cfe26a8d5a9fbe99032
andela-fokosun <okosunuzflorence@gmail.com>
2017-04-24 16:46:58 +0100

    rename file

---
+++ types/types.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// boolean declaration
+	var button bool
+
+	// string declaration
+	var greeting string
+
+	button = true
+	greeting = "Hello World"
+
+	fmt.Println(button)
+	fmt.Println(greeting)
+}
