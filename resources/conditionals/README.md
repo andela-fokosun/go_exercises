23d755491dfafff7cf8ddcc4469a278c8af01775
andela-fokosun <okosunuzflorence@gmail.com>
2017-06-10 00:38:10 +0100

    if statement in go

---
+++ resources/conditionals/README.md
@@ -0,0 +1,11 @@
+# If
+
+Go's if statements are like its for loops; the expression need not be surrounded by parentheses ( ) but the braces { } are required.
+
+## If with a short statement
+
+Like for, the if statement can start with a short statement to execute before the condition.
+
+Variables declared by the statement are only in scope until the end of the if.
+
+(Try using v in the last return statement.)
---
+++ resources/conditionals/conditionals.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(sqrt(2), sqrt(-4))
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
+// Go's if statements need not be surrounded by parentheses ( )
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+// Variables declared by the statement are only in scope until the end of the if
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
